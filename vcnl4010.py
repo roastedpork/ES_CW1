1462ea396cf331f51100c6029993beb473809ad8
Bingjie <jienet888@gmail.com>
2017-01-26 15:27:42 +0000

    optimized some code

--- vcnl4010.py
+++ vcnl4010.py
@@ -10,10 +10,8 @@ class Sensor:
 		self.PROX_CONFIG = 0x82		# Rate of proximity measurement
 		self.LED_CONFIG = 0x83 		# LED used to measure proximity 
 		self.LIGHT_CONFIG = 0x84	# Ambient light measurement parameters
-		self.LIGHT_ADD_HI = 0x85	#
-		self.LIGHT_ADD_LO = 0x86
-		self.PROX_ADD_HI = 0x87
-		self.PROX_ADD_LO = 0x88
+		self.LIGHT_ADD = 0x85		# 0x85 and 0x86 are MSB and LSB of light reading, big endian 	
+		self.PROX_ADD = 0x87		# 0x87 and 0x88 are MSB and LSB of proximity reading, big endian
 
 		self.sensor.writeto_mem(self.ID, self.PROX_CONFIG, str(0x00).encode())
 		self.sensor.writeto_mem(self.ID, self.LIGHT_CONFIG, str(0x9D).encode())
@@ -24,35 +22,26 @@ class Sensor:
 
 	def getRawLight(self):
 		self.takeReading()
-		hi = self.sensor.readfrom_mem(self.ID, self.LIGHT_ADD_HI,1)
-		lo = self.sensor.readfrom_mem(self.ID, self.LIGHT_ADD_LO,1)
-		hi , lo = bytearray(hi)[0], bytearray(lo)[0]
+
+		combined = self.sensor.readfrom_mem(self.ID, self.LIGHT_ADD,2)
+		hi, lo = bytearray(combined)
 		hi <<= 8
 		return hi + lo
 
 	def getRawProx(self):
 		self.takeReading()
 
-		status_bits = bytearray(self.sensor.readfrom_mem(self.ID, self.CMD_REG,1))[0]
-
-		if not status_bits & 0x20:
-			print("Proximity sensor did not measure a reading")
-			return None
-
-		hi = self.sensor.readfrom_mem(self.ID, self.PROX_ADD_HI,1)
-		lo = self.sensor.readfrom_mem(self.ID, self.PROX_ADD_LO,1)
-		hi , lo = bytearray(hi)[0], bytearray(lo)[0]
+		combined = self.sensor.readfrom_mem(self.ID, self.PROX_ADD,2)
+		hi, lo = bytearray(combined)
 		hi <<= 8
 		return hi + lo
 
 def main():
 	sensor = Sensor()
 	readout = sensor.getRawLight()
-
-	print(readout)
+	print("Light sensor raw data reading:", readout)
 	readout = sensor.getRawProx()
-
-	print(readout)
+	print("Proximity sensor raw data reading:",readout)
 
 if __name__ == "__main__":
 	main()
