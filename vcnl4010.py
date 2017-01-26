162a25fb135b04cae91e58f1cff79e9065cf07a4
Bingjie <jienet888@gmail.com>
2017-01-26 15:34:39 +0000

    optimized even more code

--- vcnl4010.py
+++ vcnl4010.py
@@ -6,33 +6,34 @@ class Sensor:
 		self.sensor = machine.I2C(scl = machine.Pin(5), sda = machine.Pin(4), freq = 100000)
 
 		self.CMD_REG = 0x80			# Measurement enable bits
-
 		self.PROX_CONFIG = 0x82		# Rate of proximity measurement
 		self.LED_CONFIG = 0x83 		# LED used to measure proximity 
 		self.LIGHT_CONFIG = 0x84	# Ambient light measurement parameters
+		self.DATA_ADD = 0x85
 		self.LIGHT_ADD = 0x85		# 0x85 and 0x86 are MSB and LSB of light reading, big endian 	
 		self.PROX_ADD = 0x87		# 0x87 and 0x88 are MSB and LSB of proximity reading, big endian
 
 		self.sensor.writeto_mem(self.ID, self.PROX_CONFIG, str(0x00).encode())
 		self.sensor.writeto_mem(self.ID, self.LIGHT_CONFIG, str(0x9D).encode())
 
-	def takeReading(self):
+		self.reading = None
+
+	def read(self):
 		buf = str(0xff).encode()
 		self.sensor.writeto_mem(self.ID, self.CMD_REG, buf)
 
-	def getRawLight(self):
-		self.takeReading()
+		combined = self.sensor.readfrom_mem(self.ID, self.DATA_ADD,4)
+		self.reading = bytearray(combined)
 
-		combined = self.sensor.readfrom_mem(self.ID, self.LIGHT_ADD,2)
-		hi, lo = bytearray(combined)
+	def getRawLight(self):
+		self.read()
+		hi, lo = self.reading[0], self.reading[1]
 		hi <<= 8
 		return hi + lo
 
 	def getRawProx(self):
-		self.takeReading()
-
-		combined = self.sensor.readfrom_mem(self.ID, self.PROX_ADD,2)
-		hi, lo = bytearray(combined)
+		self.read()
+		hi, lo = self.reading[2], self.reading[3]
 		hi <<= 8
 		return hi + lo
 
