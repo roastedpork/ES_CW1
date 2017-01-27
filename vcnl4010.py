38e91cb18b7ad09313383787c4b8638f71426c7f
Bingjie <jienet888@gmail.com>
2017-01-27 13:27:15 +0000

    renamed variables

--- vcnl4010.py
+++ vcnl4010.py
@@ -3,7 +3,7 @@ import time
 class Sensor:
 	def __init__(self):
 		self.ID = 0x13
-		self.sensor = machine.I2C(scl = machine.Pin(5), sda = machine.Pin(4), freq = 100000)
+		self.i2c = machine.I2C(scl = machine.Pin(5), sda = machine.Pin(4), freq = 100000)
 
 		self.CMD_REG = 0x80			# Measurement enable bits
 		self.PROX_CONFIG = 0x82		# Rate of proximity measurement
@@ -13,9 +13,9 @@ class Sensor:
 		# self.LIGHT_ADD = 0x85		# 0x85 and 0x86 are MSB and LSB of light reading, big endian 	
 		# self.PROX_ADD = 0x87		# 0x87 and 0x88 are MSB and LSB of proximity reading, big endian
 		self.IRQ_CTRL = 0x89
-		self.sensor.writeto_mem(self.ID, self.PROX_CONFIG, str(0x00).encode())
-		self.sensor.writeto_mem(self.ID, self.LIGHT_CONFIG, str(0x9D).encode())
-		self.sensor.writeto_mem(self.ID, self.IRQ_CTRL, str(0x02).encode())
+		self.i2c.writeto_mem(self.ID, self.PROX_CONFIG, str(0x00).encode())
+		self.i2c.writeto_mem(self.ID, self.LIGHT_CONFIG, str(0x9D).encode())
+		self.i2c.writeto_mem(self.ID, self.IRQ_CTRL, str(0x02).encode())
 
 		self.reading = None
 
@@ -23,24 +23,24 @@ class Sensor:
 
 	def read(self):
 		buf = str(0xff).encode()
-		self.sensor.writeto_mem(self.ID, self.CMD_REG, buf)
+		self.i2c.writeto_mem(self.ID, self.CMD_REG, buf)
 
 		if self.first_read:
 			time.sleep_us(1470)
 			self.first_read = False
 
-		combined = self.sensor.readfrom_mem(self.ID, self.DATA_ADD,4)
+		combined = self.i2c.readfrom_mem(self.ID, self.DATA_ADD,4)
 		self.reading = bytearray(combined)
 
 	def getRawLight(self):
 		self.read()
-		hi, lo = self.reading[0], self.reading[1]
+		hi, lo = self.reading[0:2]
 		hi <<= 8
 		return hi + lo
 
 	def getRawProx(self):
 		self.read()
-		hi, lo = self.reading[2], self.reading[3]
+		hi, lo = self.reading[2:4]
 		hi <<= 8
 		return hi + lo
 
